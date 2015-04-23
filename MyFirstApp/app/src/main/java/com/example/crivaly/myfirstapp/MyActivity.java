4c66c20b0cff9e8e9a92c58243aee955fd798e36
crivaly <crivaly@icloud.com>
2015-04-23 12:55:04 +0200

    cambios en el texto de mi primera aplicación

--- MyFirstApp/app/src/main/java/com/example/crivaly/myfirstapp/MyActivity.java
+++ MyFirstApp/app/src/main/java/com/example/crivaly/myfirstapp/MyActivity.java
@@ -1,9 +1,15 @@
 package com.example.crivaly.myfirstapp;
 
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
 
 
 public class MyActivity extends ActionBarActivity {
@@ -12,6 +18,11 @@ public class MyActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
     }
 
 
@@ -35,5 +46,21 @@ public class MyActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_my, container, false);
+            return rootView;
+        }
     }
 }
